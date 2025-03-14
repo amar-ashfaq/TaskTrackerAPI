9c5e9fcb5a9ae83039dedb2e953f8a78e15a818e
Amar AA <amar.ashfaq95@gmail.com>
2025-03-14 17:15:27 +0000

    Add project files.

---
+++ TaskTrackerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTrackerAPI.Models;
+
+namespace TaskTrackerAPI
+{
+    public class TaskTrackerDbContext : DbContext
+    {
+        public TaskTrackerDbContext(DbContextOptions<TaskTrackerDbContext> options) : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
