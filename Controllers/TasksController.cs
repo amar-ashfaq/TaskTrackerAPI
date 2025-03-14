2869844c4b4c3ad1319ae966df4c1e37f25de78e
Amar AA <amar.ashfaq95@gmail.com>
2025-03-14 22:32:47 +0000

    Created the Get, Create, and Get All endpoints

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -1,12 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskTrackerAPI.Models;
 
 namespace TaskTrackerAPI.Controllers
 {
     public class TasksController : Controller
     {
-        public IActionResult Index()
+        private readonly TaskTrackerDbContext _taskTrackerDbContext;
+        public TasksController(TaskTrackerDbContext taskTrackerDbContext)
         {
-            return View();
+            _taskTrackerDbContext = taskTrackerDbContext;
         }
+
+        // GET
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskItem>>> GetTaskItems()
+        {
+            var taskItems = await _taskTrackerDbContext.Tasks
+                .OrderBy(x => x.Id)
+                .ToListAsync();
+
+            return Ok(taskItems);
+        }
+
+        // GET
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<TaskItem> GetTaskItem(int id)
+        {          
+            var taskItem = _taskTrackerDbContext.Tasks.Find(id);
+
+            if (taskItem == null) {
+                return NotFound();
+            }
+
+            return Ok(taskItem);          
+        }
+
+        // CREATE
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] TaskItem taskItem) 
+        {
+            if (taskItem == null)
+            {
+                return BadRequest("Task data is required.");
+            }
+
+            _taskTrackerDbContext.Tasks.Add(taskItem);
+            await _taskTrackerDbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTaskItem), new { id = taskItem.Id }, taskItem);
+        }
+
     }
 }
